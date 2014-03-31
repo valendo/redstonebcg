cbc4b7305a7be283c7d19357b8fcb4fc2c389cb4
Tai Vo <vothanhtai86@gmail.com>
2014-03-31 09:51:23 +0700

    add module duplicator

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'redstone');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'mysql');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
